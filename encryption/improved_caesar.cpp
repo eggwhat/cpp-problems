c5d402fc4df726d6c089967c2d6674bc466603e7
padamczykpiotr <Piotr.Padamczyk@n-able.com>
2024-07-03 15:43:21 +0200

    (#3) improve caesar algorithm

--- encryption/improved_caesar.cpp
+++ encryption/improved_caesar.cpp
@@ -1,11 +1,15 @@
 #include "improved_caesar.h"
 
+int euclideanModulo(int a, int b) {
+    return ((a) < 0 ? ((((a) % (b)) + (b)) % (b)) : ((a) % (b)));
+}
+
 namespace improved_encryption {
     std::string ImprovedCaesar::encrypt(std::string userInput) {
         srand(ImprovedCaesar::seed);
         for (char& ch: userInput) {
-            ch = static_cast<char>((ch + rand() % ImprovedCaesar::shift - ImprovedCaesar::ascii_offset)
-                % ImprovedCaesar::ascii_modulo + ImprovedCaesar::ascii_offset);
+            ch = static_cast<char>(euclideanModulo((ch + rand() % ImprovedCaesar::ascii_offset - ImprovedCaesar::ascii_offset),
+                ImprovedCaesar::ascii_modulo) + ImprovedCaesar::ascii_offset);
         }
         return userInput;
     }
@@ -13,8 +17,8 @@ namespace improved_encryption {
     std::string ImprovedCaesar::decrypt(std::string encryptedMessage) {
         srand(ImprovedCaesar::seed);
         for (char& ch: encryptedMessage) {
-            ch = static_cast<char>((ch - rand() % ImprovedCaesar::shift - ImprovedCaesar::ascii_offset)
-                % ImprovedCaesar::ascii_modulo + ImprovedCaesar::ascii_offset);
+            ch = static_cast<char>(euclideanModulo((ch - rand() % ImprovedCaesar::ascii_offset - ImprovedCaesar::ascii_offset),
+                ImprovedCaesar::ascii_modulo) + ImprovedCaesar::ascii_offset);
         }
         return encryptedMessage;
     }
