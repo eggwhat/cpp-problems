c42da1a14bf710c24fda8f809bc76df2829ac392
padamczykpiotr <Piotr.Padamczyk@n-able.com>
2024-07-03 14:52:52 +0200

    (#2) add selection sort

--- derived_types/main.cpp
+++ derived_types/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+void printArray(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    };
+    std::cout << std::endl;
+}
+
+void insertionSort(int* arr, int size) {
+    for(int i = 0; i < size; i++) {
+        int min = arr[i];
+        for(int j = i + 1; j < size; j++) {
+            if (min > arr[j])
+                min = arr[j];
+        }
+        std::swap(arr[i], min);
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const int arraySize = 5;
+    int a[arraySize] = {2, 1, 3, 7, 0};
+    printArray(a, arraySize);
+    a[4] = 2137;
+    insertionSort(a, arraySize);
+    printArray(a, arraySize);
+
     return 0;
 }
