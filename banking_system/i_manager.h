792bd3651f4e96e723f0828109ef55641e4a074f
padamczykpiotr <Piotr.Padamczyk@n-able.com>
2024-07-05 15:05:49 +0200

    (#4) update manager interface

--- banking_system/i_manager.h
+++ banking_system/i_manager.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <string>
+#include <memory>
+
+#include "i_account.h"
+
 namespace bank {
     class IManager {
     private:
     public:
         virtual ~IManager() = 0;
+
+        virtual std::string getAccountDetails() = 0;
+        virtual std::unique_ptr<IAccount> createAccount() = 0;
+        virtual void depositMoney() = 0;
+        virtual void withdrawMoney() = 0;
     };
 }
