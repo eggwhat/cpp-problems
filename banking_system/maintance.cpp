e210a5755db7e29abf4867d78cc63af9f685a974
padamczykpiotr <Piotr.Padamczyk@n-able.com>
2024-07-08 12:02:33 +0200

    (#4) update maintance class

--- banking_system/maintance.cpp
+++ banking_system/maintance.cpp
@@ -1,4 +1,13 @@
 #include "maintance.h"
 
 namespace bank {
+    Maintance::Maintance() {
+        m_clients = std::vector<std::shared_ptr<Person>>();
+        m_accounts = std::vector<std::unique_ptr<IAccount>>();
+    }
+
+    std::unique_ptr<IAccount> Maintance::maintance(Person const &person) {
+
+    }
+
 } // bank
--- banking_system/maintance.h
+++ banking_system/maintance.h
@@ -1,7 +1,18 @@
 #pragma once
 
+#include <vector>
+
+#include "person.h"
+#include "i_account.h"
+
 namespace bank {
     class Maintance {
+    private:
+        std::vector<std::shared_ptr<Person>> m_clients;
+        std::vector<std::unique_ptr<IAccount>> m_accounts;
+    public:
+        Maintance();
 
+        std::unique_ptr<IAccount> maintance(Person const& person);
     };
 }
