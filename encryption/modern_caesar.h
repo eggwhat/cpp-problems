660a5f97cbd04b5443f9b0689dce9348371bee97
padamczykpiotr <Piotr.Padamczyk@n-able.com>
2024-07-08 10:27:51 +0200

    (#3) fix naming of private member

--- encryption/modern_caesar.h
+++ encryption/modern_caesar.h
@@ -6,10 +6,10 @@ namespace encryption {
 
     class ModernCaesar: public IEncryption {
     private:
-        int const seed;
+        int const m_seed;
     public:
-        explicit ModernCaesar(int _seed)
-            : seed(_seed) {
+        explicit ModernCaesar(int seed)
+            : m_seed(seed) {
         }
         std::string encrypt(std::string const& userInput) const override;
         std::string decrypt(std::string const& encryptedMessage) const override;
